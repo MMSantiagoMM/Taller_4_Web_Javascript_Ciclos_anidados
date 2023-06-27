424f897f2560895282b7bfa9cea0e4ef8d6fff69
MMSantiagoMM <santiagomosquera2@gmail.com>
2023-06-27 18:55:08 -0500

    Cambios al ciclo for

--- 10_for/index.js
+++ 10_for/index.js
@@ -1,23 +1,21 @@
 /* Cree un programa que permite registrar el número de compras que se venden cada día. pedir al usuario cuantos productos va a comprar y registrar el precio unitario, calcular el valor total de cada compra y al finalizar el día calcular cuántas facturas se hicieron y el total general. */
 
-let Cantidad = parseInt(prompt("Cuántos productos desea ingresar para este cliente? \n Si no desea ingresar productos presione cancelar"));
-let totalPrecios = 0;
-let cont = 0;
-for (let i = 1; i <= Cantidad; i++) {
+let compras = parseInt(prompt("Cuántos compras desea ingresar?"))
+let totalGeneral = 0;
+let contador = 0;
+for(let i = 1; i <= compras; i++){
     let totalCompra = 0;
-    document.write("Cliente# " + i + "<br>")
-    document.write("Este cliente compró " + Cantidad + " productos" + "<br>")
-    for (let j = 1; j <= Cantidad; j++) {
-        let Precios = parseInt(prompt("Precio del producto: " + j))
-        document.write("El valor de producto # " + j + " es de: " + Precios + "<br>")
-        totalCompra+= Precios
+    document.write(`Cliente # ${i} <br>`)
+    let productos = parseInt(prompt(`Ingrese la cantidad de productos para el cliente #${i} <br>`))
+    for(let j = 1; j <= productos; j++){
+        let precio = parseInt(prompt(`Ingrese el precio del producto #${j}`))
+        document.write(`El precio del producto #${j} es de ${precio} <br>`)
+        totalCompra += precio;
+        contador++;
     }
-    totalPrecios+=totalCompra;
-    document.write("El precio total de la compra de este cliente es de: " + totalCompra + " <br> <br> <br>")
-    Cantidad = parseInt(prompt("Cuántos productos desea ingresar para este cliente? \n Si no desea ingresar productos presione cancelar"));
-    cont++;
+    document.write(`El total de esta compra fue de ${totalCompra} <br> <br>`)
+    totalGeneral += totalCompra;
 }
-document.write("Al final del día se hicieron " + cont + " compras" + "<br>")
-document.write("El total general de las ventas del día fueron de: " + totalPrecios)
+document.write(`La cantidad de facturas del día fue de ${contador} <br> El valor general de las compras del día fue de ${totalGeneral}`);
 
 
